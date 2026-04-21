bf5150686e1924df65d5b18c6d6dc1e48752969f
Bimidu Gunathilake <118644309+Bimidu@users.noreply.github.com>
2026-04-21 07:20:12 +0530

    Add async rewrites for API requests

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,15 @@ const nextConfig = {
       { protocol: 'https', hostname: 'picsum.photos', pathname: '/**' },
     ],
   },
-}
 
-export default nextConfig
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://ctse-api-gateway-lb-1666361828.eu-north-1.elb.amazonaws.com/:path*',
+      },
+    ];
+  },
+};
+
+export default nextConfig;
